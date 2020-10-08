d62c67c32a36552b5dfa1b27c12e6d2a1d684731
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-08 15:36:36 +0600

    Installed redux & react-redux and created a counter component and also created three folders named 'services', 'actions', 'reducers' for redux files

---
+++ src/components/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Counter = () => {
+    return (
+        <div>
+            <h3>Count: 0</h3>
+            <button>Increament</button>
+            <button>Decreament</button>
+        </div>
+    )
+}
+
+export default Counter
