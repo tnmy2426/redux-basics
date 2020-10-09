09faff28dafdfac9ee1e8994a113097facbec437
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-09 12:13:52 +0600

    Redux- connecting redux reducers, actions with react component using connect() function from react-redux and successfully completed counter component to work

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {increament, decreament} from '../services/actions/counterAction'
 
-const Counter = () => {
+const Counter = ({ increament, decreament, count}) => {
     return (
         <div>
-            <h3>Count: 0</h3>
-            <button>Increament</button>
-            <button>Decreament</button>
+            <h3>Count: {count}</h3>
+            <button onClick={increament}>Increament</button>
+            <button onClick={decreament}>Decreament</button>
         </div>
     )
 }
 
-export default Counter
+const mapStateToProps = (state) =>({
+    count: state.counterReducer.count
+})
+
+export default connect(mapStateToProps, {increament, decreament})(Counter)
