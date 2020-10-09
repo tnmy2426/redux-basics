75ff672504df1dc4ff2f729f15b6cb8776f5b7ae
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-09 14:15:05 +0600

    Redux- added redux-devtools extention in google chorme and also installed redux-devtools-extension [npm install redux-devtools-extension] and used that tools in store file with composeWithDevTolls method.Finally visualize our data in redux state

--- src/services/store.js
+++ src/services/store.js
@@ -2,10 +2,12 @@ import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers'
 import logger from 'redux-logger'
 
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 const initialState = {}
 
 export const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(logger)
+    composeWithDevTools(applyMiddleware(logger))
 )
